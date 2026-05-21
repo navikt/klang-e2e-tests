ca35a96f62a2d2a2d68b0cda90b0ce22bb364f94
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-05-21 14:03:17 +0200

    Remove redundant playwright deps, unify imports to @playwright/test

--- tests/accessibility.test.ts
+++ tests/accessibility.test.ts
@@ -1,5 +1,5 @@
 import AxeBuilder from '@axe-core/playwright';
-import { expect } from 'playwright/test';
+import { expect } from '@playwright/test';
 import { Innsendingsytelse } from '@/fixtures/innsendingsytelse';
 import { test } from '@/fixtures/registrering/fixture';
 import { Type } from '@/fixtures/registrering/klang-case';
